144581d805f13488834f868603d5fc2be95b94a6
Wahyu Kristianto <w.kristories@gmail.com>
2018-10-09 22:09:10 +0700

    Added options

--- src/Qrcode.php
+++ src/Qrcode.php
@@ -2,6 +2,7 @@
 
 namespace Kristories\Qrcode;
 
+use Cache;
 use Laravel\Nova\Fields\Field;
 
 class Qrcode extends Field
@@ -12,4 +13,37 @@ class Qrcode extends Field
      * @var string
      */
     public $component = 'qrcode';
+
+    public function text($text = null)
+    {
+        return $this->withMeta(['text' => $text]);
+    }
+
+    public function background($background = null)
+    {
+
+        return $this->withMeta(['background' => $this->_renderImage($background)]);
+    }
+
+    public function logo($logo = null)
+    {
+        return $this->withMeta(['logo' => $this->_renderImage($logo)]);
+    }
+
+    protected function _renderImage($url = null)
+    {
+        if ($url and curl_init($url)) {
+            $image = Cache::rememberForever('qr-img-' . md5($url), function () use ($url) {
+                $image     = file_get_contents($url);
+                $file_info = new \finfo(FILEINFO_MIME_TYPE);
+                $mime_type = $file_info->buffer($image);
+
+                return 'data: ' . $mime_type . ';base64,' . base64_encode(file_get_contents($url));
+            });
+
+            return $image;
+        }
+
+        return false;
+    }
 }
