4b9c3328aa81830d6f5d995f4c59e275bb6fc298
Wahyu Kristianto <w.kristories@gmail.com>
2018-10-09 22:08:52 +0700

    Setup index

---
+++ resources/js/components/IndexField.vue
@@ -0,0 +1,31 @@
+<template>
+    <span>
+    	<vue-qr 
+            class="qr-item" 
+            :text="this.field.text" 
+            size="50" 
+            :bgSrc="this.field.background"
+            :logoSrc="this.field.logo"
+            margin="0"
+        ></vue-qr>
+    </span>
+</template>
+
+<script>
+export default {
+    props: [
+    	'resourceName', 
+    	'field'
+    ],
+}
+</script>
+
+<style>
+.qr-item{
+	margin-top: 5px;
+}
+.qr-item img{
+    border: solid thin #ddd;
+    padding: 2px;
+}
+</style>
