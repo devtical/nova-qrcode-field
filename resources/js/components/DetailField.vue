7dc853bf6808b33419927cee6d9bffa1f86dc251
Wahyu Kristianto <w.kristories@gmail.com>
2018-10-09 22:08:44 +0700

    Setu detail

---
+++ resources/js/components/DetailField.vue
@@ -0,0 +1,27 @@
+<template>
+    <panel-item :field="field">
+        <p slot="value">
+            <vue-qr 
+                class="qr-item" 
+                :text="this.field.text" 
+                size="200" 
+                :bgSrc="this.field.background"
+                :logoSrc="this.field.logo"
+                margin="0"
+            ></vue-qr>
+        </p>
+    </panel-item>
+</template>
+
+<script>
+export default {
+    props: ['field'],
+}
+</script>
+
+<style>
+.qr-item img{
+    border: solid thin #ddd;
+    padding: 5px;
+}
+</style>
