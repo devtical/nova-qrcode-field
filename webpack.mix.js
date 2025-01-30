40411e6ebe00dec10b09f37242848c9a773f862a
Wahyu Kristianto <w.kristories@gmail.com>
2025-01-30 18:11:26 +0700

    Update packages

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,11 @@
 let mix = require('laravel-mix')
+let NovaExtension = require('laravel-nova-devtool')
 
-require('./nova.mix')
+mix.extend('nova', new NovaExtension())
 
 mix
   .setPublicPath('dist')
   .js('resources/js/field.js', 'js')
   .vue({ version: 3 })
   .nova('devtical/qrcode')
+  .version()
